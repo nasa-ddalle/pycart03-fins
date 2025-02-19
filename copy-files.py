98491d83964ff27fbcb5d1592270bf3cfa705102
Derek J. Dalle <derek.j.dalle@nasa.gov>
2025-02-19 13:17:49 -0800

    Add script to copy files

---
+++ copy-files.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Standard library modules
+import os
+import shutil
+
+
+# This folder
+fdir = os.path.dirname(os.path.realpath(__file__))
+# Name of working folder
+fwork = os.path.join(fdir, "work")
+
+# Files list
+fnames = [
+    "fins.json",
+    "inputs/bullet.tri",
+    "inputs/fin1.tri",
+    "inputs/fin2.tri",
+    "inputs/fin3.tri",
+    "inputs/fin4.tri",
+    "inputs/arrow.xml",
+    "inputs/matrix.csv"
+]
+
+# Check if working folder exists
+if not os.path.isdir(fwork):
+    # Create it
+    os.mkdir(fwork)
+
+# Loop through files
+for fname in fnames:
+    # System independence
+    f1 = fname.replace("/", os.sep)
+    # Source file
+    fsrc = os.path.join(fdir, f1)
+    # Destination file
+    fdest = os.path.join(fwork, f1)
+    # Check if output file already exists
+    if os.path.isfile(fdest):
+        # Skip
+        continue
+    # Break out folder names (if any)
+    fparts = fname.split("/")[:-1]
+    # Create cumulative subfolder name
+    fsub = fwork
+    # Loop through subfolder names
+    for fpart in fparts:
+        # Add to subfolder name
+        fsub = os.path.join(fsub, fpart)
+        # Check if folder exists
+        if os.path.isdir(fsub):
+            continue
+        # Create folder
+        os.mkdir(fsub)
+    # Copy file
+    shutil.copy(fsrc, fdest)
+
